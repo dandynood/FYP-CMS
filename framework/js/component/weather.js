17e5d8e5aaba8cc87f1ed95be8a992b179b5276e
Jacky <yuehan1997@gmail.com>
2018-05-14 16:50:52 +0800

    Fix-Bugs

---
+++ framework/js/component/weather.js
@@ -0,0 +1,11 @@
+/*jslint white:true */
+/*global angular */
+/*jslint plusplus:true*/
+angular.module('mainApp').component('weather', {
+    templateUrl: 'template/weather.html',
+     //cache: false,
+    
+    controller: function ($scope) {
+        "use strict";
+    }
+});
